b1adb1abf42845593550d6695ff905aa4173a828
FullMetalPr0gr1mmer <Kareem.hosny2001@gmail.com>
2025-06-19 12:48:07 +0300

    UpdatingSimilarityEngine

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from src.parser.parser import * #imports
 from src.text_cleaner.text_cleaner import *
-
+from src.matcher.similarity_engine import *
 resumes = extract_all_resumes("data/resumes")
-for name,text in resumes.items():
+jobs = load_job_descriptions("data/jobs")
+'''for name,text in resumes.items():
     print(f"----{name}----\n {preprocess_text(text)}...\n")
 
-    jobs = load_job_descriptions("data/jobs")
+    
     for name,text in jobs.items():
-        print(f"----{name}----\n {preprocess_text(text)}...\n")
+        print(f"----{name}----\n {preprocess_text(text)}...\n")'''
+
+
+processed_resumes ={name:preprocess_text(text) for name,text in resumes.items()}
+processed_jobs ={name:preprocess_text(text) for name,text in jobs.items()}
+similarities = compute_similarity(processed_resumes, processed_jobs)
+
+top_matches = rank_matches(similarities,2)
+for resume, job_scores in top_matches.items():
+    print(f"Resume: {resume}")
+    for job, score in job_scores:
+        print(f"  -> Match with '{job}': {score}")
+    print()
--- src/matcher/similarity_engine.py
+++ src/matcher/similarity_engine.py
@@ -8,11 +8,27 @@ def compute_similarity(resume_texts,job_texts):
     all_texts =list(resume_texts.values())+list(job_texts.values())
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(all_texts)
+    '''learn vocab and idf, returns document-term matrix
+     equivalent to fit followed by transform yet more efficient'''
 
     resume_vectors=tfidf_matrix[:len(resume_texts)]
     job_vectors=tfidf_matrix[len(resume_texts):]
     similarity_scores = {}
-    
+    for i,(resume_name,_) in enumerate(resume_texts.items()):
+        scores={}
+        for j,(job_name,_) in enumerate(job_texts.items()):
+            score=cosine_similarity(resume_vectors[i],job_vectors[j])[0][0] #sklearn Cosine similarity returns 2D vectors
+            # thats why we use [0][0]to access the single float value
+            scores[job_name]=round(score,4)
+        similarity_scores[resume_name]=scores
+    return similarity_scores
 
-    '''learn vocab and idf, returns document-term matrix
-     equivalent to fit followed by transform yet more efficient'''
+def rank_matches(similarity_scores,top_k=3):
+    ranked_results={}
+    for resume,scores in similarity_scores.items():
+        sorted_scored=sorted(scores.items(),key=lambda x:x[1],reverse=True)
+
+        ranked_results[resume]=sorted_scored[:top_k]
+    return ranked_results
+
+
